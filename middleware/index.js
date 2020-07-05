2e1f74756f065f72a222129a2a7e49ef1e91b335
Keith McKenna <keithmckenna@gmail.com>
2020-07-05 23:53:04 +0000

    start creation of middleware to check if user is signed in and is admin

--- middleware/index.js
+++ middleware/index.js
@@ -1,2 +1,23 @@
+var User = require("../models/user");
+
 // all the middleware goes here
 var middlewareObj = {};
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You will need to be logged in to do that.");
+    res.redirect("/login");
+};
+
+middlewareObj.isUserAdmin = function(req, res, next) {
+	if(req.isAuthenticated() && req.user.isAdmin){
+		return next();
+	}
+    req.flash("error", "Apologies, you don't have permission to do that.");
+    res.redirect("/blog");
+};
+
+
+module.exports = middlewareObj;
