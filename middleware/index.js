ad1184340ae1f978d7d6b0a769de12ee61b4c601
Keith McKenna <keithmckenna@gmail.com>
2020-07-06 02:06:20 +0000

    add auth middleware

--- middleware/index.js
+++ middleware/index.js
@@ -3,20 +3,12 @@ var User = require("../models/user");
 // all the middleware goes here
 var middlewareObj = {};
 
-middlewareObj.isLoggedIn = function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "You will need to be logged in to do that.");
-    res.redirect("/login");
-};
-
-middlewareObj.isUserAdmin = function(req, res, next) {
+middlewareObj.isLoggedIn = function(req, res, next) {
 	if(req.isAuthenticated() && req.user.isAdmin){
 		return next();
 	}
-    req.flash("error", "Apologies, you don't have permission to do that.");
-    res.redirect("/blog");
+    req.flash("error", "Sorry, you\'ll need to be logged in to proceed.");
+    res.redirect("/");
 };
 
 
