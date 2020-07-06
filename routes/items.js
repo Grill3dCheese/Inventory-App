75d22897c28023b94f269bdfe83ac805381c9505
Keith McKenna <keithmckenna@gmail.com>
2020-07-06 22:38:19 +0000

    require nodemailer package in index route, add forgot password routing and set up nodemailer parameters -- begin set up of items routing, created index route for inventory/items page

--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,17 @@
 var express = require("express");
 var router = express.Router();
-var Inventory = require("../models/inventory");
+var Inventory = require("../models/item");
 var middleware = require("../middleware");
+
+// index of inventory page
+router.get("/inventory", function(req, res){
+    Inventory.find({}, function(err, items){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("inventory/index", {items: items, page: "items"});
+        }
+    });
+});
+
+module.exports = router;
