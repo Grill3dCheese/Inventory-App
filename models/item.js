277bb9b78722e40446226cc9ac943d844e916e58
Keith McKenna <keithmckenna@gmail.com>
2020-07-06 22:35:59 +0000

    create item schema and export as Item

---
+++ models/item.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+// schema setup
+var itemSchema = new mongoose.Schema({
+    name: String,
+	quantity: Number,
+    category: String,
+    image: String,
+    description: String,
+    createdAt: { type: Date, default: Date.now },
+    author: {
+        id : {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+        username: String
+        },
+});
+
+module.exports = mongoose.model("Item", itemSchema);
