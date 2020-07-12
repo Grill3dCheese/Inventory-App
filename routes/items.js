c3e0f67217756ec5e60d655cc007783480b1128d
Keith McKenna <keithmckenna@gmail.com>
2020-07-12 01:53:42 +0000

    create post route to create new item and save to DB, create route to new item form, routing to show specific item's page also created - set up error routing/messaging as well as success routing/messaging for all routes

--- routes/items.js
+++ routes/items.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var router = express.Router();
-var Inventory = require("../models/item");
+var Item = require("../models/item");
 var middleware = require("../middleware");
 
 // index of inventory page
 router.get("/inventory", middleware.isLoggedIn, function(req, res){
-    Inventory.find({}, function(err, items){
+    Item.find({}, function(err, items){
         if(err){
             console.log(err);
         } else {
@@ -16,6 +16,49 @@ router.get("/inventory", middleware.isLoggedIn, function(req, res){
 
 // Add item to inventory database routing
 // CREATE - add new item
-router.post("/", middleware.isLoggedIn)
+router.post("/", middleware.isLoggedIn, function(req, res){
+	// get data from form and add to items array
+	var name = req.body.name;
+	var quantity = req.body.quantity;
+	var category = req.body.category;
+	var tag = req.body.tag;
+	var image = req.body.image;
+	var desc = req.body.description;
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	};
+	var newItem = {name: name, quantity: quantity, category: category, tag: tag, image: image, description: desc, author: author};
+	// Create a new item and save to DB
+	Item.create(newItem, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			// redirect back to inventory page
+			console.log(newlyCreated);
+			req.flash("success", "Success! A new item has been added to inventory.");
+			res.redirect("/inventory");
+		}
+	});
+});
+
+// New Item
+router.get("/new", middleware.isLoggedIn, function(req, res){
+	res.render("inventory/new");
+});
+
+// Show Item
+router.get("/:id", function(req, res){
+	Item.findById(req.params.id).populate("comments").exec(function(err, foundItem){
+		if(err || !foundItem){
+			console.log(err);
+			req.flash("error", "Sorry, the item you are looking for does not exist.");
+			res.redirect("/inventory");
+		} else {
+			console.log(foundItem);
+			res.render("inventory/show", {item: foundItem});
+		}
+	});
+});
 
 module.exports = router;
