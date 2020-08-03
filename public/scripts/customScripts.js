0ef39fcc0925bc5eb653c97ca2c5b77dcc321944
Keith McKenna <keithmckenna@gmail.com>
2020-08-03 21:10:51 +0000

    V1.0 - Clean custom javascript code and re-order - reduction in pixels on scrollUp class

--- public/scripts/customScripts.js
+++ public/scripts/customScripts.js
@@ -12,40 +12,6 @@ window.setTimeout(function () {
 	});
 }, 2000);
 
-function openNav() {
-	navLinks.style.width = '270px';
-	document.body.style.marginLeft = '270px';
-	document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
-	return;
-}
-
-function closeNav() {
-	navLinks.style.width = '0';
-	document.body.style.marginLeft = '0';
-	document.body.style.backgroundColor = '#edf2f7';
-	return;
-}
-
-function timeCheck() {
-	if (curHr < 12) {
-		greet.innerText = '🌅 \n Good Morning,';
-	} else if (curHr < 18) {
-		greet.innerText = '☀️ \n Good Afternoon,';
-	} else {
-		greet.innerText = '🌙 \n Good Evening,';
-	}
-}
-
-navButton.addEventListener('click', timeCheck);
-navButton.addEventListener('click', openNav);
-sideLinks.forEach((item) => {
-	item.addEventListener('click', closeNav);
-});
-mainPage.addEventListener('click', closeNav);
-
-$(document).ready(function () {
-	'use strict';
-
 	// hides navbar upon scrolling down and then reappears upon scrolling up
 
 	var c,
@@ -76,4 +42,34 @@ $(document).ready(function () {
 			$(this).removeClass('active').addClass('active');
 		}
 	});
+
+function openNav() {
+	navLinks.style.width = '270px';
+	document.body.style.marginLeft = '270px';
+	document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+	return;
+}
+
+function closeNav() {
+	navLinks.style.width = '0';
+	document.body.style.marginLeft = '0';
+	document.body.style.backgroundColor = '#edf2f7';
+	return;
+}
+
+function timeCheck() {
+	if (curHr < 12) {
+		greet.innerText = '🌅 \n Good Morning,';
+	} else if (curHr < 18) {
+		greet.innerText = '☀️ \n Good Afternoon,';
+	} else {
+		greet.innerText = '🌙 \n Good Evening,';
+	}
+}
+
+navButton.addEventListener('click', timeCheck);
+navButton.addEventListener('click', openNav);
+sideLinks.forEach((item) => {
+	item.addEventListener('click', closeNav);
 });
+mainPage.addEventListener('click', closeNav);
